1cee50932de6dd8b4a1b47846a6f7debe6623cb9
pcg <panchenguang@qq.com>
2019-05-05 15:54:56 +0800

    send text message ok
    every bot can send text message

--- main.go
+++ main.go
@@ -8,17 +8,18 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
 	"log"
+	"strconv"
 )
 
 const (
-	//address = "localhost:6061"//"192.168.8.122:6061"
-	address = "192.168.8.122:6061"
+	address = "localhost:6061"//"192.168.8.122:6061"
+	//address = "192.168.8.122:6061"
 )
-
-func SendClientHi(nmc *pb.Node_MessageLoopClient) error{
+//Hi
+func sendClientHi(nmc *pb.Node_MessageLoopClient) error{
 	reqBody := new(ClientComMessage)
 	clientHi:=new(MsgClientHi)
-	clientHi.Id = "69990"
+	//clientHi.Id = "69990"
 	clientHi.UserAgent="TinodeWeb/0.15.14 (Chrome/73.0; Win32); tinodejs/0.15.14"
 	clientHi.Version="0.15.14"
 	clientHi.DeviceID="L1iC2"
@@ -36,14 +37,15 @@ func SendClientHi(nmc *pb.Node_MessageLoopClient) error{
 	fmt.Println(serverMsg)
 	return err
 }
-
-func SendClientLogin(nmc *pb.Node_MessageLoopClient){
+//登录
+func sendClientLogin(nmc *pb.Node_MessageLoopClient,username string, passwd string ){
 	reqBody := new(ClientComMessage)
 	clientLogin:=new(MsgClientLogin)
-	clientLogin.Id = "69991"
+	//clientLogin.Id = "69991"
 	clientLogin.Scheme="basic"
 	//encodedString:=base64.URLEncoding.EncodeToString([]byte("alice:alice123"))
-	encodedString:="alice:alice123"
+	//encodedString:="alice:alice123"
+	encodedString:=username+":"+passwd
 	fmt.Println(encodedString)
 	clientLogin.Secret=[]byte(encodedString)
 	reqBody.Login=clientLogin
@@ -58,31 +60,62 @@ func SendClientLogin(nmc *pb.Node_MessageLoopClient){
 	}
 	fmt.Println(serverMsg)
 }
-
-func sendClientSub(nmc *pb.Node_MessageLoopClient) error{
+//进入编组
+func sendClientSub(nmc *pb.Node_MessageLoopClient,topic_name string) error{
 	reqBody := new(ClientComMessage)
 	clientSub:=new(MsgClientSub)
-	clientSub.Id = "69992"
-	clientSub.Set(
-	//encodedString:=base64.URLEncoding.EncodeToString([]byte("alice:alice123"))
-	encodedString:="alice:alice123"
-	fmt.Println(encodedString)
-	clientLogin.Secret=[]byte(encodedString)
-	reqBody.Login=clientLogin
-	jsons,_:=json.Marshal(*clientLogin)
-	fmt.Println(string(jsons))
-	//.SayHello(context.Background(), reqBody)
+	clientSub.Topic=topic_name
+	//add set
+	clientMsgSetQuery:=new(MsgSetQuery)
+	clientMsgSetSub:=new(MsgSetSub)
+	clientMsgSetSub.Mode="JRWPS"
+	clientMsgSetQuery.Sub=clientMsgSetSub
+	clientSub.Set=clientMsgSetQuery
+	//add sub
+	clientMsgGetQuery:=new(MsgGetQuery)
+	clientMsgGetOpts:=new(MsgGetOpts)
+	clientMsgGetOpts.Limit=24
+	clientMsgGetQuery.Data=clientMsgGetOpts
+	clientMsgGetQuery.What="data sub desc"
+
+	clientSub.Set=clientMsgSetQuery
+	clientSub.Get=clientMsgGetQuery
+
+	reqBody.Sub=clientSub
+	//send
 	err:=(*nmc).Send(pbCliSerialize(reqBody))
+	//recv
 	serverMsg,err:=(*nmc).Recv()
 	if err != nil{
 		fmt.Print(err)
-		return
+		return err
 	}
 	fmt.Println(serverMsg)
+	return nil
+}
+//发送消息
+func sendClientPub(nmc *pb.Node_MessageLoopClient,topic_name string,msg string) error{
+	reqBody := new(ClientComMessage)
+	clientPub:=new(MsgClientPub)
+	clientPub.Topic=topic_name
+	clientPub.NoEcho=true
+	clientPub.Content=msg
+
+	reqBody.Pub=clientPub
+	//send
+	err:=(*nmc).Send(pbCliSerialize(reqBody))
+	//recv
+	serverMsg,err:=(*nmc).Recv()
+	if err != nil{
+		fmt.Print(err)
+		return err
+	}
+	fmt.Println(serverMsg)
+	return nil
 }
 
 func main() {
-	fmt.Println("starting the golang chatbot.....")
+	/*fmt.Println("starting the golang chatbot.....")
 	// Set up a connection to the gRPC server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
@@ -96,9 +129,52 @@ func main() {
 	if err != nil {
 		grpclog.Fatalln(err)
 	}
-	SendClientHi(&nmc)
-	SendClientLogin(&nmc)
+	//初始化交互
+	sendClientHi(&nmc)
+	//登录
+	sendClientLogin(&nmc,"alice","alice123")
+	//切入编组
+	sendClientSub(&nmc,"grp_3cPvkpRTy8")
+	//发送消息
+	sendClientPub(&nmc,"grp_3cPvkpRTy8","Hi,I am Bot1.")*/
+
+	testManyConnection()
 
 	select {} // 阻塞
+}
+
+func loginToServer(username string,passwd string,index int){
+	fmt.Println("starting the golang chatbot "+strconv.Itoa(index))
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("server connected with:"+address)
+	// Creates a new CustomerClient
+	client:=pb.NewNodeClient(conn)
+	nmc, err := client.MessageLoop(context.Background())
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+	sendClientHi(&nmc)
+	sendClientLogin(&nmc,username,passwd)
+	sendClientSub(&nmc,"grpQOnSKtB1iPo")
+	sendClientPub(&nmc,"grpQOnSKtB1iPo","Hi,I am Bot "+strconv.Itoa(index))
+}
+
+
+func testManyConnection(){
+	var userMap=map[string]string{}
+	userMap["alice"]="alice123"
+	userMap["bob"]="bob123"
+	userMap["carol"]="carol123"
+	userMap["dave"]="dave123"
+	userMap["frank"]="frank123"
+	var index int
+	for key,value:=range userMap{
+		loginToServer(key,value,index)
+		index++
+	}
 }
 
